7899ac88713926601b123c55e8e5cccdbddd55f0
Zayver <zayber.re@gmail.com>
2023-10-12 14:49:17 -0500

    Add CORS config

--- apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package co.edu.javeriana.pedisoft.apigateway.config;
 
+import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,8 +18,18 @@ public class SecurityConfig {
         //TODO Enable security
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(ServerHttpSecurity.CorsSpec::disable)
                 .authorizeExchange(ex -> ex.anyExchange().permitAll())
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        val config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        val source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
 }
