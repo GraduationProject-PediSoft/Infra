edae3a626361ba5112ca9bf7752abb0c537393ea
Zayver <zayber.re@gmail.com>
2023-10-25 12:06:00 -0500

    Update CORS

--- apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package co.edu.javeriana.pedisoft.apigateway.config;
 
+import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,7 +24,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http){
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(ex -> ex.pathMatchers(HttpMethod.POST, "/files").denyAll()
+                .authorizeExchange(ex -> ex.pathMatchers(HttpMethod.POST, "/files/").denyAll()
                         //This validation is done by the usermanager microservice, so here is not necessary to check
                         .pathMatchers("/user/**").permitAll()
                         .anyExchange().authenticated()
@@ -29,5 +35,19 @@ public class SecurityConfig {
                         )
                 )
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        val corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://tesis.10.43.101.226.nip.io")); // Set your allowed origins here
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        val source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
