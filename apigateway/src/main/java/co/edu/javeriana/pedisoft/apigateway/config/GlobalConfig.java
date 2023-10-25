947f9772303bf2a8b2c34c9aa129dff579ec500a
Zayver <zayber.re@gmail.com>
2023-10-25 16:12:44 -0500

    Add NimbusReactiveJWTDecoder

---
+++ apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/GlobalConfig.java
@@ -0,0 +1,17 @@
+package co.edu.javeriana.pedisoft.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+
+@Configuration
+public class GlobalConfig {
+    @Value("${issuerURI}")
+    private String issuerURI;
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder(){
+        return NimbusReactiveJwtDecoder.withIssuerLocation(issuerURI).build();
+    }
+}
--- apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/co/edu/javeriana/pedisoft/apigateway/config/SecurityConfig.java
@@ -1,27 +1,25 @@
 package co.edu.javeriana.pedisoft.apigateway.config;
 
 import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
-
-import java.util.List;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Value("${issuerURI}")
-    private String issuerURI;
+    @Autowired
+    private NimbusReactiveJwtDecoder nimbusReactiveJwtDecoder;
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http){
         return http
@@ -33,7 +31,7 @@ public class SecurityConfig {
                 )
                 .oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(
-                                it -> it.jwtDecoder(JwtDecoders.fromIssuerLocation(issuerURI))
+                                it -> it.jwtDecoder(nimbusReactiveJwtDecoder)
                         )
                 )
                 .build();
